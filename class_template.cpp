32e7150e78cb486ffa4b08d6ff3692ef90b7bc35
SiangCh <67034864+Sinowa-Programming@users.noreply.github.com>
2023-10-26 14:42:43 -0400

    Add class template for encryption/decryption

---
+++ class_template.cpp
@@ -0,0 +1,17 @@
+// Include statements here
+#include <iostream>
+
+using namespace std;
+
+class ClassName
+{
+    public:
+        ClassName();
+        void Encrypt(string filename);
+        void Decrypt(string filename, type key); // change 'type' to desired type
+
+    private:
+    // Functions here
+};
+
+
